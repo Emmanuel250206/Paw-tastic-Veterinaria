21d5caf8d6e49bba68e9c8a25564e0a793e28a54
Emmanuel Abraham <hdzrenteria@gmail.com>
2026-03-26 12:07:07 -0600

    Modificacion de login
    Se modifico el espacio entre los TextField de nombre y contraseña.

--- src/main/java/com/mycompany/aplicacion/App.java
+++ src/main/java/com/mycompany/aplicacion/App.java
@@ -9,49 +9,50 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * JavaFX App   
+ * JavaFX App
  */
 public class App extends Application {
 
     private static Scene scene;
 
-@Override
-public void start(Stage stage) throws IOException {
-    Parent root = loadFXML("fxml/VeterinariaP1"); 
-    scene = new Scene(root);
-    stage.setScene(scene);
-    
-    // El Login no debe estirarse
-    stage.setResizable(false); 
-    stage.sizeToScene(); 
-    stage.show();
-}
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = loadFXML("fxml/VeterinariaP1");
+        scene = new Scene(root);
+        stage.setScene(scene);
+
+        // El Login no debe estirarse
+        stage.setResizable(false);
+        stage.sizeToScene();
+        stage.show();
+    }
 
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
-private static Parent loadFXML(String fxml) throws IOException {
-    // Usamos el cargador de clases del sistema para buscar el recurso
-    java.net.URL url = App.class.getResource("/" + fxml + ".fxml");
-    
-    if (url == null) {
-        // Esto nos dirá exactamente qué ruta está fallando en la consola
-        throw new IOException("¡No encontré el archivo! Busqué en: /com/mycompany/aplicacion/" + fxml + ".fxml");
+    private static Parent loadFXML(String fxml) throws IOException {
+        // Usamos el cargador de clases del sistema para buscar el recurso
+        java.net.URL url = App.class.getResource("/" + fxml + ".fxml");
+
+        if (url == null) {
+            // Esto nos dirá exactamente qué ruta está fallando en la consola
+            throw new IOException("¡No encontré el archivo! Busqué en: /com/mycompany/aplicacion/" + fxml + ".fxml");
+        }
+
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
+        return fxmlLoader.load();
     }
-    
-    FXMLLoader fxmlLoader = new FXMLLoader(url);
-    return fxmlLoader.load();
-}
 
-public static Scene getScene() {
-    return scene;
-}
+    public static Scene getScene() {
+        return scene;
+    }
 
-public static Stage getStage() {
-    return (Stage) scene.getWindow();
-}
-    
-    
-}
+    public static Stage getStage() {
+        return (Stage) scene.getWindow();
+    }
 
+    public static void main(String[] args) {
+        launch();
+    }
+} // Fin de la clase

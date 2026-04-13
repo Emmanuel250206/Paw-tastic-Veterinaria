7657c8372428e04381c15a94ba2436bdfbe589fa
Emmanuel Abraham <hdzrenteria@gmail.com>
2026-04-13 16:20:23 -0600

    Fix 1.2

--- src/main/java/com/mycompany/aplicacion/persistencia/Conexion.java
+++ src/main/java/com/mycompany/aplicacion/persistencia/Conexion.java
@@ -7,12 +7,12 @@ public class Conexion {
     private Connection conectar;
 
     private String usuario = "root";
-    private String contrasenia = "";
+    private String contrasenia = "TuPasswordSegura123!";
     private String bd = "pawtastic";
     private String ip = "localhost";
     private String puerto = "3306";
 
-    private String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
+    private String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
     public Connection estableceConexion() {
         try {
